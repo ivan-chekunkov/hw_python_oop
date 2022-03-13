a6dc5fe08b750baf0044dd71a51822b5a6542366
Ivan Chekunkov <chekunkov.ivan@yandex.ru>
2022-03-13 19:39:40 +0300

    Comments

--- homework.py
+++ homework.py
@@ -2,15 +2,20 @@ import datetime as dt
 
 
 class Calculator:
+    """Родительский класс для калькуляторов."""
 
     def __init__(self, limit):
         self.limit = limit
         self.records = []
 
     def add_record(self, rec):
+        """Добавление записи данных в калькулятор."""
+
         self.records.append(rec)
 
     def get_today_stats(self):
+        """Возвращает статистику за день."""
+
         day_sum = 0
         now = dt.datetime.now().date()
         for record in self.records:
@@ -19,6 +24,8 @@ class Calculator:
         return day_sum
 
     def get_week_stats(self):
+        """Возвращает статистику за неделю."""
+
         week_sum = 0
         date_now = dt.datetime.now().date()
         date_delta = date_now - dt.timedelta(weeks=1)
@@ -29,10 +36,14 @@ class Calculator:
         return week_sum
 
     def get_cash_remained(self):
+        """Возвращает остаток от лимита."""
+
         return self.limit - self.get_today_stats()
 
 
 class Record:
+    """Класс записи данных в калькулятор."""
+
     FORMAT_DATE = '%d.%m.%Y'
 
     def __init__(self, amount, comment, date=None):
@@ -45,8 +56,11 @@ class Record:
 
 
 class CaloriesCalculator(Calculator):
+    """Калькулятор калорий."""
 
     def get_calories_remained(self):
+        """Возвращает статус разрешённых калорий на сегодняшний день."""
+
         cal_day_remained = self.get_cash_remained()
         if cal_day_remained > 0:
             return ('Сегодня можно съесть что-нибудь ещё, '
@@ -56,19 +70,24 @@ class CaloriesCalculator(Calculator):
 
 
 class CashCalculator(Calculator):
-    USD_RATE = 70.00
-    EURO_RATE = 80.00
+    """Калькулятор денег."""
+
+    USD_RATE = 60.00
+    EURO_RATE = 70.00
     RUB_RATE = 1.00
 
     def get_today_cash_remained(self, currency):
-        sum_day_remained = self.get_cash_remained()
-        if sum_day_remained == 0:
-            return 'Денег нет, держись'
+        """Возвращает статус оставшихся денег на сегодняшний день."""
+
         all_currency: dict = {
             'rub': (self.RUB_RATE, 'руб'),
             'eur': (self.EURO_RATE, 'Euro'),
             'usd': (self.USD_RATE, 'USD')
         }
+        sum_day_remained = self.get_cash_remained()
+        if sum_day_remained == 0:
+            return 'Денег нет, держись'
+
         out = abs(sum_day_remained / all_currency[currency][0])
         currency_type = all_currency[currency][1]
         if sum_day_remained > 0:
